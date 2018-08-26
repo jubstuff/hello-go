da589617096fbba377f2984b777ad2221b20dde5
Giustino Borzacchiello <giustinob@gmail.com>
2018-08-26 19:07:46 +0200

    Check for non-number input in GetNumber

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -18,8 +19,23 @@ func GetLine(prompt string) string {
 
 // GetNumber returns a number
 func GetNumber(prompt string) int {
+	var input string
 	var number int
-	fmt.Println(prompt + "\n?> ")
-	fmt.Scanln(&number)
+	var err error
+
+	isOk := false
+
+	for !isOk {
+		fmt.Println(prompt + "\n?> ")
+		fmt.Scanln(&input)
+		number, err = strconv.Atoi(input)
+		if err == nil {
+			isOk = true
+		} else {
+			fmt.Printf("%q doesn't look like a number. Please insert a number.\n", input)
+		}
+
+	}
+
 	return number
 }
